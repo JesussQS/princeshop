6a2cf3271558acc254f4de56d7da60c5fa0b0b4e
JesussQS <jesusqs_42@hotmail.com>
2022-10-20 17:34:38 -0500

    Producto Migracion

---
+++ Data/Migrations/20221020223327_ProductoMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace princeshop.Data.Migrations
+{
+    public partial class ProductoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
