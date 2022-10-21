f144e0ee070693fef3ac5eaed9c8a8d88f43b456
JesussQS <jesusqs_42@hotmail.com>
2022-10-21 17:06:31 -0500

    Detalle producto y creacion de proforma

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -15,12 +15,14 @@ namespace princeshop.Controllers
     public class CatalogoController : Controller
     {
         private readonly ILogger<CatalogoController> _logger;
-        private readonly ApplicationDbContext _context; 
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager; 
 
-        public CatalogoController(ILogger<CatalogoController> logger,ApplicationDbContext context)
+        public CatalogoController(ILogger<CatalogoController> logger,ApplicationDbContext context,UserManager<IdentityUser> userManager)
         {
             _logger = logger;
             _context = context;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index(string? searchString){
@@ -32,6 +34,32 @@ namespace princeshop.Controllers
             }
       
             return View(await productos.ToListAsync());
+        }
+        public async Task<IActionResult> Details(int? id){
+            Producto objProduct = await _context.DataProductos.FindAsync(id);
+            if(objProduct == null){
+                return NotFound();
+            }
+            return View(objProduct);
+        }
+         public async Task<IActionResult> Add(int? id,string? talla){
+            var userID = _userManager.GetUserName(User);
+            if(userID==null){
+                ViewData["Message"]="Por favor debe loguearse antes de agregar un producto";
+                List<Producto> productos= new List<Producto>();
+                return View("Index",productos);
+            }else{
+                var producto = await _context.DataProductos.FindAsync(id);    
+                Proforma proforma = new Proforma();
+                proforma.Producto = producto;
+                proforma.Precio = producto.Precio;
+                proforma.Cantidad = 1;
+                proforma.UserID = userID;
+                proforma.Talla=talla;
+                _context.Add(proforma);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
