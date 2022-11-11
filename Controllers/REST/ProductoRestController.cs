0edb010e20d76c294b89e9bd6063062544d11da7
JesussQS <jesusqs_42@hotmail.com>
2022-11-11 00:32:03 -0500

    Peticiones http

---
+++ Controllers/REST/ProductoRestController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using princeshop.Data;
+using princeshop.Models;
+
+namespace princeshop.Controllers.REST
+{
+    [ApiController]
+    [Route("api/producto")]
+    public class ProductoRestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public ProductoRestController(ApplicationDbContext context){
+             _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Producto> ListProductos()
+        {
+             var listProductos=_context.DataProductos.OrderBy(s => s.Id).ToList();   
+             return listProductos.ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public Producto GetProduct(int? id)
+        {
+            var producto =  _context.DataProductos.Find(id);
+            return producto;
+        }
+
+        [HttpPost]
+        public Producto CreateProduct(Producto producto){
+            _context.Add(producto);
+            _context.SaveChanges();
+            return producto;
+        }
+    }
+}
